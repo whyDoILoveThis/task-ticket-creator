93b9d36c236b8e553e9035cfefbb7a059a945220
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2025-09-11 15:46:50 -0500

    Update page

--- app/page.jsx
+++ app/page.jsx
@@ -100,9 +100,9 @@ const Dashboard = () => {
           </div>
         );
       })
-      ) : (
+      ) : 
         activeTab === "edit" &&
-        projects.map((project) => (
+        (projects.map((project) => (
           <div key={project._id} className="mb-6">
             <span className="flex gap-2 items-center mt-16">
               <span className="text-3xl font-extrabold mb-2">
